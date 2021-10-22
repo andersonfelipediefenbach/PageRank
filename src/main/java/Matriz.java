0703f6118d82dccd7126a0c26a959f3bc6e6e844
Anderson Felipe Diefenbach <anderson_diefenbach@terceiros.sicredi.com.br>
2021-10-21 23:29:55 -0300

    Initial commit

---
+++ src/main/java/Matriz.java
@@ -0,0 +1,39 @@
+public class Matriz {
+
+    String documento;
+    String linha;
+    Integer valor;
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+    public Matriz(String documento,String linha, Integer valor) {
+        this.documento = documento;
+        this.linha = linha;
+        this.valor = valor;
+    }
+
+
+
+    public String getLinha() {
+        return linha;
+    }
+
+    public void setLinha(String linha) {
+        this.linha = linha;
+    }
+
+}
